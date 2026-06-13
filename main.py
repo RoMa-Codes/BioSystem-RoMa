77af9c0e38aef52db91a946c841af0545ce5c785
ML3220 <mlluc.lmarc@gmail.com>
2026-06-13 12:22:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ screen=pygame.display.set_mode([screen_width, screen_height])
 running = True
 circle_x = 350 
 circle_y = 200
+creatures_size = 100
 animal_sprite = pygame.image.load("slug.png")
-sprite_animal = pygame.transform.scale(animal_sprite, (100, 100))
+sprite_animal = pygame.transform.scale(animal_sprite, (creatures_size, creatures_size))
 speed = .5
 while running:
     # Look for events (mouse clicks, key presses)
@@ -29,11 +30,11 @@ while running:
     teclas = pygame.key.get_pressed()
     if teclas[pygame.K_LEFT] and circle_x > 0:
         circle_x -= speed
-    if teclas[pygame.K_RIGHT] and circle_x < screen_width - 100:
+    if teclas[pygame.K_RIGHT] and circle_x < screen_width - creatures_size:
         circle_x += speed
     if teclas[pygame.K_UP] and circle_y > 0:
         circle_y -= speed
-    if teclas[pygame.K_DOWN] and circle_y < screen_height - 100:       
+    if teclas[pygame.K_DOWN] and circle_y < screen_height - creatures_size:       
         circle_y += speed
     # Update the actual display
     pygame.display.flip()
