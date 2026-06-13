1685174bd0d9cec58fafecfbd664d3fa6f1be47f
ML3220 <mlluc.lmarc@gmail.com>
2026-06-13 21:11:38 +0200

    Refactor main.py to add herbivorous creatures

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import random
 import os
+import math
 #makes this work
 pygame.init()
 
@@ -26,22 +27,69 @@ food_timer = 0
 # Creant aliment
 class Food:
     def __init__(self):
-        food = random.randint(1, 10)
-        if food < 11:
+        Identity = random.randint(1, 10)
+        if Identity < 11:
             self.nutrients = 100
             self.name = "Apple"
-        elif food > 1 and food < 6:
+        elif Identity > 1 and food < 6:
             self.nutrients = 30
             self.name = "Bush"
-        elif food > 5:
+        elif Identity > 5:
             self.nutrients = 5
             self.name = "Plant"
         self.x = random.randint(0, screen_width)
         self.y = random.randint(0, screen_height)
 foods = []
 
+
+class herb_eating_Creatures:
+    def __init__(self):
+        # Stats del Deer
+        self.needed_nutrients = 50
+        self.repro_nutrients = 80
+        self.want_nutrients = 100
+
+        self.norm_speed = 3
+        self.panic_speed = 4
+
+        self.radius = 100      # rango de movimiento
+        self.sight = 50
+
+        self.name = "Deer"
+        self.hp = 100
+
+        # posición base
+        self.base_x = random.randint(0, screen_width)
+        self.base_y = random.randint(0, screen_height)
+
+        # posición actual
+        self.x = self.base_x
+        self.y = self.base_y
+
+        self.timer = 0
+
+def update(self):
+    self.timer += 1
+
+    if self.timer >= 30:
+         self.timer = 0
+
+    while True:
+        dx = random.randint(-self.radius, self.radius)
+        dy = random.randint(-self.radius, self.radius)
+
+        if dx*dx + dy*dy <= self.radius*self.radius:
+            self.x = self.base_x + dx
+            self.y = self.base_y + dy
+            break
+    
+        self.x = random.randint(0, screen_width)
+        self.y = random.randint(0, screen_height)
+animals_list = []
 for i in range(0):
     foods.append(Food())
+for i in range(5):
+    animals_list.append(herb_eating_Creatures())
 
 # Obtiene la carpeta donde reside main.py
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -74,7 +122,9 @@ while running:
     
     for food in foods:
         if food.name == "Apple":
-            screen.blit(apple_sprite, (food.x, food.y)) 
+            screen.blit(apple_sprite, (food.x, food.y))
+    for a in animals_list:
+        screen.blit(sprite_animal, (a.x, a.y))
     
     screen.blit(sprite_animal, (circle_x, circle_y))
     teclas = pygame.key.get_pressed()
